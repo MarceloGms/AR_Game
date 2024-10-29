c7f8bc3f3d5c4e6ef1b6dacbe07b75799ac83179
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-10-29 14:45:17 +0000

    ta a funfar a rota

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -6,150 +6,160 @@ import BackButton from "../components/BackButton";
 import { useNavigation } from "@react-navigation/native";
 
 export default function GameScreen({ route }) {
-  const navigation = useNavigation();
+	const navigation = useNavigation();
 
-  // Use useRef para armazenar monument_coordinates
-  const monumentCoordinates = useRef({
-    latitude: route.params.latitude,
-    longitude: route.params.longitude,
-  });
+	// Use useRef para armazenar monument_coordinates
+	const monumentCoordinates = useRef({
+		latitude: route.params.latitude,
+		longitude: route.params.longitude,
+	});
 
-  const [location, setLocation] = useState(null);
-  const [region, setRegion] = useState(null);
-  const [routeCoordinates, setRouteCoordinates] = useState([]); // Para armazenar as coordenadas da rota
+	const [location, setLocation] = useState(null);
+	const [region, setRegion] = useState(null);
+	const [routeCoordinates, setRouteCoordinates] = useState([]); // Para armazenar as coordenadas da rota
 
-  useEffect(() => {
-    let locationSubscription;
+	useEffect(() => {
+		let locationSubscription;
 
-    (async () => {
-      const { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        console.log("Permission to access location was denied");
-        return;
-      }
+		(async () => {
+			const { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== "granted") {
+				console.log("Permission to access location was denied");
+				return;
+			}
 
-      // Obter posição inicial e definir como região do mapa
-      const initialLocation = await Location.getCurrentPositionAsync({});
-      setLocation(initialLocation.coords);
-      setRegion({
-        latitude: initialLocation.coords.latitude,
-        longitude: initialLocation.coords.longitude,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      });
+			// Obter posição inicial e definir como região do mapa
+			const initialLocation = await Location.getCurrentPositionAsync({});
+			setLocation(initialLocation.coords);
+			setRegion({
+				latitude: initialLocation.coords.latitude,
+				longitude: initialLocation.coords.longitude,
+				latitudeDelta: 0.01,
+				longitudeDelta: 0.01,
+			});
 
-      // Obter a rota
-      await fetchRoute(initialLocation.coords, monumentCoordinates.current);
-      locationSubscription = await Location.watchPositionAsync(
-        {
-          accuracy: Location.Accuracy.High,
-          timeInterval: 5000,
-          distanceInterval: 1,
-        },
-        (newLocation) => {
-          setLocation(newLocation.coords);
-        }
-      );
-    })();
+			// Obter a rota
+			await fetchRoute(initialLocation.coords, monumentCoordinates.current);
+			locationSubscription = await Location.watchPositionAsync(
+				{
+					accuracy: Location.Accuracy.High,
+					timeInterval: 5000,
+					distanceInterval: 1,
+				},
+				(newLocation) => {
+					setLocation(newLocation.coords);
+				}
+			);
+		})();
 
-    return () => {
-      if (locationSubscription) {
-        locationSubscription.remove();
-      }
-    };
-  }, []);
+		return () => {
+			if (locationSubscription) {
+				locationSubscription.remove();
+			}
+		};
+	}, []);
 
-  const fetchRoute = async (start, end) => {
-    console.log("START params", start);
-    console.log("END params", end);
+	const fetchRoute = async (start, end) => {
+		console.log("START params", start);
+		console.log("END params", end);
 
-    const API_KEY = "5b3ce3597851110001cf6248b348401dfa20456aacf41374d55da08d"; // Insira sua chave de API aqui
-    const url = `https://api.openrouteservice.org/v2/directions/driving-car?start=${start.longitude},${start.latitude}&end=${end.longitude},${end.latitude}`;
+		const API_KEY = "5b3ce3597851110001cf6248b348401dfa20456aacf41374d55da08d"; // Insira sua chave de API aqui
+		const url = `https://api.openrouteservice.org/v2/directions/driving-car?start=${start.longitude},${start.latitude}&end=${end.longitude},${end.latitude}`;
 
-    try {
-      const response = await fetch(url, {
-        headers: {
-          Authorization: API_KEY,
-          "Content-Type": "application/json",
-        },
-      });
+		try {
+			const response = await fetch(url, {
+				headers: {
+					Authorization: API_KEY,
+					"Content-Type": "application/json",
+				},
+			});
 
-      if (!response.ok) {
-        const errorData = await response.json();
-        console.error("API Error Response:", errorData);
-        throw new Error("Network response was not ok");
-      }
+			if (!response.ok) {
+				const errorData = await response.json();
+				console.error("API Error Response:", errorData);
+				throw new Error("Network response was not ok");
+			}
 
-      const data = await response.json();
-      console.log("API Response Data:", data);
+			const data = await response.json();
+			console.log("API Response Data:", data);
 
-      // Extrair coordenadas da rota
-      if (data.features && data.features.length > 0) {
-        const coords = data.features[0].geometry.coordinates.map((coord) => ({
-          latitude: coord[1],
-          longitude: coord[0],
-        }));
-        setRouteCoordinates(coords); // Atualizar as coordenadas da rota
-      } else {
-        console.log("No routes found");
-      }
-    } catch (error) {
-      console.error("Error fetching route:", error);
-    }
-  };
+			// Extrair coordenadas da rota
+			if (data.features && data.features.length > 0) {
+				const coords = data.features[0].geometry.coordinates.map((coord) => ({
+					latitude: coord[1],
+					longitude: coord[0],
+				}));
+				setRouteCoordinates(coords); // Atualizar as coordenadas da rota
+			} else {
+				console.log("No routes found");
+			}
+		} catch (error) {
+			console.error("Error fetching route:", error);
+		}
+	};
 
-  return (
-    <View style={styles.container}>
-      {region && (
-        <MapView
-          style={styles.map}
-          region={region}
-          onRegionChangeComplete={(newRegion) => setRegion(newRegion)}
-        >
-          {location && (
-            <Marker
-              coordinate={{
-                latitude: location.latitude,
-                longitude: location.longitude,
-              }}
-              title="Minha Localização"
-              description="Estou aqui"
-            />
-          )}
+	return (
+		<View style={styles.container}>
+			{region && (
+				<MapView
+					style={styles.map}
+					region={region}
+					onRegionChangeComplete={(newRegion) => setRegion(newRegion)}
+					camera={{
+						center: {
+							latitude: location.latitude,
+							longitude: location.longitude,
+						},
+						pitch: 45, // Tilt angle
+						heading: 0, // Camera heading
+						altitude: 1000, // Camera altitude
+						zoom: 15, // Zoom level
+					}}
+				>
+					{location && (
+						<Marker
+							coordinate={{
+								latitude: location.latitude,
+								longitude: location.longitude,
+							}}
+							title="Minha Localização"
+							description="Estou aqui"
+						/>
+					)}
 
-          {/* Marker do monumento */}
-          <Marker
-            coordinate={monumentCoordinates.current}
-            title="Monumento"
-            description="Destino"
-          />
+					{/* Marker do monumento */}
+					<Marker
+						coordinate={monumentCoordinates.current}
+						title="Monumento"
+						description="Destino"
+					/>
 
-          {/* Adicionar a Polyline*/}
-          {routeCoordinates.length > 0 && (
-            <Polyline
-              coordinates={routeCoordinates}
-              strokeColor="#5c2f00"
-              strokeWidth={6}
-            />
-          )}
-        </MapView>
-      )}
+					{/* Adicionar a Polyline*/}
+					{routeCoordinates.length > 0 && (
+						<Polyline
+							coordinates={routeCoordinates}
+							strokeColor="#5c2f00"
+							strokeWidth={6}
+						/>
+					)}
+				</MapView>
+			)}
 
-      <View className="px-4 absolute bottom-8">
-        <BackButton
-          onPress={() => navigation.goBack()}
-          icon="arrow-back-outline"
-        />
-      </View>
-    </View>
-  );
+			<View className="px-4 absolute bottom-8">
+				<BackButton
+					onPress={() => navigation.goBack()}
+					icon="arrow-back-outline"
+				/>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
+	container: {
+		flex: 1,
+	},
+	map: {
+		...StyleSheet.absoluteFillObject,
+	},
 });
--- screens/StarterScreen.js
+++ screens/StarterScreen.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import {
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  Dimensions,
-  ScrollView,
-  Pressable,
-  Modal,
-  Button,
+	Text,
+	View,
+	SafeAreaView,
+	Image,
+	Dimensions,
+	ScrollView,
+	Pressable,
+	Modal,
+	Button,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
@@ -24,127 +24,142 @@ import BackButton from "../components/BackButton";
 const { width, height } = Dimensions.get("window");
 
 export default function StarterScreen() {
-  const navigation = useNavigation();
-  const [selectedMonument, setSelectedMonument] = useState(null);
-  const [modalVisible, setModalVisible] = useState(false);
+	const navigation = useNavigation();
+	const [selectedMonument, setSelectedMonument] = useState(null);
+	const [modalVisible, setModalVisible] = useState(false);
 
-  const cards = [
-    [
-      {
-        id: 1,
-        name: "Sé Nova de Coimbra",
-        image: "Sé Nova de Coimbra",
+	const cards = [
+		[
+			{
+				id: 1,
+				name: "Se nova ",
+				image: TorreDeBelem,
+				description: "The Tower of Belém is a fortified tower...",
+				latitude: 38.6916,
+				longitude: -9.216,
+			},
+			{
+				id: 2,
+				name: "Mosteiro da Batalha",
+				image: MosteiroDaBatalha,
+				description: "The Monastery of Batalha is a Dominican convent...",
+			},
+		],
+		[
+			{
+				id: 3,
+				name: "Eiffel Tower",
+				image: TorreEiffel,
+				description: "The Eiffel Tower is a wrought-iron lattice tower...",
+			},
+			{
+				id: 4,
+				name: "Colosseum",
+				image: Colosseum,
+				description: "The Colosseum is an elliptical amphitheatre...",
+			},
+		],
+		[
+			{
+				id: 5,
+				name: "Arco do Triunfo",
+				image: ArcoDoTriunfo,
+				description: "The Arc de Triomphe de l'Étoile is one of the most...",
+			},
+			{
+				id: 6,
+				name: "Padrão dos Descobrimentos",
+				image: PadraoDosDescobrimentos,
+				description: "The Monument to the Discoveries is a monument...",
+			},
+		],
+	];
 
-        latitude: 40.2105,
-        longitude: -8.4267,
-      },
-      {
-        id: 2,
-        name: "Mosteiro da Batalha",
-        image: MosteiroDaBatalha,
-      },
-    ],
-    [
-      {
-        id: 3,
-        name: "Eiffel Tower",
-        image: TorreEiffel,
-      },
-      {
-        id: 4,
-        name: "Colosseum",
-        image: Colosseum,
-      },
-    ],
-    [
-      {
-        id: 5,
-        name: "Arco do Triunfo",
-        image: ArcoDoTriunfo,
-      },
-      {
-        id: 6,
-        name: "Padrão dos Descobrimentos",
-        image: PadraoDosDescobrimentos,
-      },
-    ],
-  ];
+	const openModal = (monument) => {
+		setSelectedMonument(monument);
+		setModalVisible(true);
+	};
 
-  return (
-    <ScrollView className="flex-1 bg-[#5c2f00] min-w-full">
-      <View>
-        <SafeAreaView className="flex-1 justify-between min-h-screen items-start">
-          <View className="gap-y-5 min-w-full">
-            <View className="flex justify-center items-center">
-              <Text className="text-3xl text-center text-zinc-100 font-bold">
-                Starter Screen
-              </Text>
-            </View>
+	const closeModal = () => {
+		setModalVisible(false);
+		setSelectedMonument(null);
+	};
 
-            {cards.map((card) => (
-              <View
-                key={card[0].id}
-                className="flex flex-row justify-around items-center "
-              >
-                {card.map((monument) => (
-                  <Pressable
-                    key={monument.id}
-                    onPress={() => openModal(monument)}
-                    className="flex-1"
-                  >
-                    <View className="flex flex-col items-center">
-                      <Image
-                        className="rounded-lg border-4 border-zinc-100 shadow-[5px_2px_28px_21px_#c9c9c9]"
-                        source={monument.image}
-                        style={{ width: width / 2.2, height: height / 5 }}
-                      />
-                    </View>
-                  </Pressable>
-                ))}
-              </View>
-            ))}
-          </View>
+	return (
+		<ScrollView className="flex-1 bg-[#5c2f00] min-w-full">
+			<View>
+				<SafeAreaView className="flex-1 justify-between min-h-screen items-start">
+					<View className="gap-y-5 min-w-full">
+						<View className="flex justify-center items-center">
+							<Text className="text-3xl text-center text-zinc-100 font-bold">
+								Starter Screen
+							</Text>
+						</View>
 
-          {/* Modal for Monument Info */}
-          <Modal
-            animationType="slide"
-            transparent={true}
-            visible={modalVisible}
-            onRequestClose={closeModal}
-          >
-            <View className="flex-1 justify-center items-center bg-black bg-opacity-50">
-              <View className="w-3/4 bg-white p-5 rounded-lg">
-                {selectedMonument && (
-                  <>
-                    <Text className="text-xl font-bold text-center mb-4">
-                      {selectedMonument.name}
-                    </Text>
-                    <Button
-                      title="Start"
-                      onPress={() => {
-                        navigation.navigate("GameScreen", {
-                          name: selectedMonument.name,
-                          latitude: monument.latitude,
-                          longitude: monument.longitude,
-                        });
-                        closeModal();
-                      }}
-                    />
-                    <Button title="Close" onPress={closeModal} color="red" />
-                  </>
-                )}
-              </View>
-            </View>
-          </Modal>
+						{cards.map((card) => (
+							<View
+								key={card[0].id}
+								className="flex flex-row justify-around items-center "
+							>
+								{card.map((monument) => (
+									<Pressable
+										key={monument.id}
+										onPress={() => openModal(monument)}
+										className="flex-1"
+									>
+										<View className="flex flex-col items-center">
+											<Image
+												className="rounded-lg border-4 border-zinc-100 shadow-[5px_2px_28px_21px_#c9c9c9]"
+												source={monument.image}
+												style={{ width: width / 2.2, height: height / 5 }}
+											/>
+										</View>
+									</Pressable>
+								))}
+							</View>
+						))}
+					</View>
 
-          <View className="px-4">
-            <BackButton
-              onPress={() => navigation.goBack()}
-              icon="arrow-back-outline"
-            />
-          </View>
-        </SafeAreaView>
-      </View>
-    </ScrollView>
-  );
+					{/* Modal for Monument Info */}
+					<Modal
+						animationType="slide"
+						transparent={true}
+						visible={modalVisible}
+						onRequestClose={closeModal}
+					>
+						<View className="flex-1 justify-center items-center bg-black bg-opacity-50">
+							<View className="w-3/4 bg-white p-5 rounded-lg">
+								{selectedMonument && (
+									<>
+										<Text className="text-xl font-bold text-center mb-4">
+											{selectedMonument.name}
+										</Text>
+										<Button
+											title="Start"
+											onPress={() => {
+												navigation.navigate("GameScreen", {
+													name: selectedMonument.name,
+													latitude: selectedMonument.latitude,
+													longitude: selectedMonument.longitude,
+												});
+												closeModal();
+											}}
+										/>
+										<Button title="Close" onPress={closeModal} color="red" />
+									</>
+								)}
+							</View>
+						</View>
+					</Modal>
+
+					<View className="px-4">
+						<BackButton
+							onPress={() => navigation.goBack()}
+							icon="arrow-back-outline"
+						/>
+					</View>
+				</SafeAreaView>
+			</View>
+		</ScrollView>
+	);
 }
