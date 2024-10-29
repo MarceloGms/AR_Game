d0bbc8e2a230feeb6b14af07f7e5379c2eb19bb4
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-10-29 14:06:55 +0000

    3d

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,70 +1,75 @@
 import React, { useEffect, useState } from "react";
-import { View, Dimensions, SafeAreaView } from "react-native";
+import { View, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
-import { StyleSheet } from "react-native";
 import BackButton from "../components/BackButton";
 import { useNavigation } from "@react-navigation/native";
 
-const { width, height } = Dimensions.get("window");
-
 export default function GameScreen() {
-  const navigation = useNavigation();
+	const navigation = useNavigation();
+	const [location, setLocation] = useState(null);
 
-  const [location, setLocation] = useState(null);
+	useEffect(() => {
+		(async () => {
+			const { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== "granted") {
+				console.log("Permission to access location was denied");
+				return;
+			}
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        console.log("Permission to access location was denied");
-        return;
-      }
+			const currentLocation = await Location.getCurrentPositionAsync({});
+			setLocation(currentLocation.coords);
+		})();
+	}, []);
 
-      const currentLocation = await Location.getCurrentPositionAsync({});
-      setLocation(currentLocation.coords);
-    })();
-  }, []);
+	return (
+		<View style={styles.container}>
+			{location && (
+				<MapView
+					style={styles.map}
+					initialRegion={{
+						latitude: location.latitude,
+						longitude: location.longitude,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421,
+					}}
+					showsBuildings={true}
+					tiltEnabled={true}
+					camera={{
+						center: {
+							latitude: location.latitude,
+							longitude: location.longitude,
+						},
+						pitch: 45, // Tilt angle
+						heading: 0, // Camera heading
+						altitude: 1000, // Camera altitude
+						zoom: 15, // Zoom level
+					}}
+				>
+					<Marker
+						coordinate={{
+							latitude: location.latitude,
+							longitude: location.longitude,
+						}}
+					/>
+				</MapView>
+			)}
 
-  return (
-    <View style={styles.container}>
-      {location && (
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        >
-          <Marker
-            coordinate={{
-              latitude: location.latitude,
-              longitude: location.longitude,
-            }}
-            title="Minha Localização"
-            description="Estou aqui"
-          />
-        </MapView>
-      )}
-
-      <View className="px-4 absolute bottom-8">
-        <BackButton
-          onPress={() => navigation.goBack()}
-          icon="arrow-back-outline"
-        />
-      </View>
-    </View>
-  );
+			<View className="px-4 absolute bottom-8">
+				<BackButton
+					onPress={() => navigation.goBack()}
+					icon="arrow-back-outline"
+				/>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: "100%",
-    height: "100%",
-  },
+	container: {
+		flex: 1,
+	},
+	map: {
+		...StyleSheet.absoluteFillObject,
+	},
 });
