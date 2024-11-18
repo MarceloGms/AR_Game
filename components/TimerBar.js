2455c591bb702f423c6a9ee47e5fa529327584ff
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-11-18 14:11:13 +0000

    fix timer bar

--- components/TimerBar.js
+++ components/TimerBar.js
@@ -2,32 +2,32 @@ import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 export default function TimerBar({ onTimerEnd }) {
-  const [timeLeft, setTimeLeft] = useState(5); // 30 seconds countdown
-  const [widthPercentage, setWidthPercentage] = useState(100); // Full width at start
+	const [timeLeft, setTimeLeft] = useState(10);
+	const [widthPercentage, setWidthPercentage] = useState(100); // Full width at start
 
-  useEffect(() => {
-    if (timeLeft === 0) {
-      onTimerEnd(); // Call onTimerEnd only when timeLeft reaches 0
-      return;
-    }
+	useEffect(() => {
+		if (timeLeft === 0) {
+			onTimerEnd(); // Call onTimerEnd only when timeLeft reaches 0
+			return;
+		}
 
-    const intervalId = setInterval(() => {
-      setTimeLeft((prevTime) => Math.max(prevTime - 1, 0));
-      setWidthPercentage((timeLeft / 30) * 100); // Calculate width based on newTime
-    }, 1000);
+		const intervalId = setInterval(() => {
+			setTimeLeft((prevTime) => Math.max(prevTime - 1, 0));
+			setWidthPercentage(((timeLeft - 1) / 10) * 100); // Calculate width based on newTime
+		}, 1000);
 
-    return () => clearInterval(intervalId); // Clean up the interval on component unmount
-  }, [timeLeft, onTimerEnd]); // Dependency array includes timeLeft
+		return () => clearInterval(intervalId); // Clean up the interval on component unmount
+	}, [timeLeft, onTimerEnd]); // Dependency array includes timeLeft
 
-  return (
-    <View className="w-4/5 h-8 bg-red-500 rounded-full mt-5 relative justify-center items-center">
-      <View
-        style={[{ width: `${widthPercentage}%` }]}
-        className="absolute top-0 left-0 h-full bg-green-500 rounded-full"
-      />
-      <Text className="text-white font-bold text-lg text-center">
-        {timeLeft} s
-      </Text>
-    </View>
-  );
+	return (
+		<View className="w-4/5 h-8 bg-red-500 rounded-full mt-5 relative justify-center items-center">
+			<View
+				style={[{ width: `${widthPercentage}%` }]}
+				className="absolute top-0 left-0 h-full bg-green-500 rounded-full"
+			/>
+			<Text className="text-white font-bold text-lg text-center">
+				{timeLeft} s
+			</Text>
+		</View>
+	);
 }
--- screens/ExerciseScreen.js
+++ screens/ExerciseScreen.js
@@ -7,101 +7,101 @@ import jumpingJack from "../assets/jumping_jack.mp4";
 import Button from "../components/Button";
 
 export default function ExerciseScreen({ route }) {
-  const [challengeStarted, setChallengeStarted] = useState(false);
-  const [timerFinished, setTimerFinished] = useState(false); // Track if timer is finished
-  const [isCompleted, setIsCompleted] = useState(false);
-  const [lastCheckpoint, setLastCheckpoint] = useState(false);
-  const navigation = useNavigation();
+	const [challengeStarted, setChallengeStarted] = useState(false);
+	const [timerFinished, setTimerFinished] = useState(false); // Track if timer is finished
+	const [isCompleted, setIsCompleted] = useState(false);
+	const [lastCheckpoint, setLastCheckpoint] = useState(false);
+	const navigation = useNavigation();
 
-  const startChallenge = () => {
-    setChallengeStarted(true);
-  };
+	const startChallenge = () => {
+		setChallengeStarted(true);
+	};
 
-  const handleCompleteExercise = () => {
-    setIsCompleted(true);
+	const handleCompleteExercise = () => {
+		setIsCompleted(true);
 
-    if (lastCheckpoint) {
-      navigation.navigate("FindWordScreen", {
-        checkpoints: route.params.checkpoints,
-        monumentName: route.params.monumentName,
-      }); // Navigate to EndScreen if this is the last checkpoint
-    } else {
-      // Otherwise, navigate back to the GameScreen
-      navigation.navigate("GameScreen", {
-        name: route.params.monumentName, // Pass the monument name to the GameScreen
-        latitude: route.params.latitude,
-        longitude: route.params.longitude,
-        isCompleted: true, // Pass isCompleted
-      });
-    }
-  };
+		if (lastCheckpoint) {
+			navigation.navigate("FindWordScreen", {
+				checkpoints: route.params.checkpoints,
+				monumentName: route.params.monumentName,
+			}); // Navigate to EndScreen if this is the last checkpoint
+		} else {
+			// Otherwise, navigate back to the GameScreen
+			navigation.navigate("GameScreen", {
+				name: route.params.monumentName, // Pass the monument name to the GameScreen
+				latitude: route.params.latitude,
+				longitude: route.params.longitude,
+				isCompleted: true, // Pass isCompleted
+			});
+		}
+	};
 
-  // Check if it's the last checkpoint only after timer finishes
-  useEffect(() => {
-    if (timerFinished) {
-      const completedCheckpoints = route.params.completedCheckpoints;
-      const checkpoints = route.params.checkpoints;
-      const nextCheckpointIndex = completedCheckpoints.length;
+	// Check if it's the last checkpoint only after timer finishes
+	useEffect(() => {
+		if (timerFinished) {
+			const completedCheckpoints = route.params.completedCheckpoints;
+			const checkpoints = route.params.checkpoints;
+			const nextCheckpointIndex = completedCheckpoints.length;
 
-      if (nextCheckpointIndex === checkpoints.length - 1) {
-        setLastCheckpoint(true);
-      }
-    }
-  }, [
-    route.params.completedCheckpoints,
-    route.params.checkpoints,
-    timerFinished,
-  ]);
+			if (nextCheckpointIndex === checkpoints.length - 1) {
+				setLastCheckpoint(true);
+			}
+		}
+	}, [
+		route.params.completedCheckpoints,
+		route.params.checkpoints,
+		timerFinished,
+	]);
 
-  return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white p-6">
-      <Text className="text-3xl font-bold text-center text-gray-800 mb-6">
-        Jumping Jack - 30s
-      </Text>
+	return (
+		<SafeAreaView className="flex-1 items-center justify-center bg-white p-6">
+			<Text className="text-3xl font-bold text-center text-gray-800 mb-6">
+				Jumping Jack - 10s
+			</Text>
 
-      {/* Video Player */}
-      <View className="w-full h-72 mb-6 rounded-lg overflow-hidden ">
-        <Video
-          source={jumpingJack}
-          resizeMode="contain"
-          shouldPlay={true}
-          isLooping={true}
-          style={{ width: "100%", height: "100%" }}
-        />
-      </View>
+			{/* Video Player */}
+			<View className="w-full h-72 mb-6 rounded-lg overflow-hidden ">
+				<Video
+					source={jumpingJack}
+					resizeMode="contain"
+					shouldPlay={true}
+					isLooping={true}
+					style={{ width: "100%", height: "100%" }}
+				/>
+			</View>
 
-      {challengeStarted && !timerFinished ? (
-        <TimerBar
-          onTimerEnd={() => {
-            setTimerFinished(true); // Set timer finished state to true when timer ends
-          }}
-        />
-      ) : null}
+			{challengeStarted && !timerFinished ? (
+				<TimerBar
+					onTimerEnd={() => {
+						setTimerFinished(true); // Set timer finished state to true when timer ends
+					}}
+				/>
+			) : null}
 
-      {/* Conditional Rendering for Buttons after timer finishes */}
-      {timerFinished &&
-        (lastCheckpoint ? (
-          <Button
-            title="Finish Game"
-            onPress={handleCompleteExercise}
-            bgColor="#0aa812"
-          />
-        ) : (
-          <Button
-            title="Voltar ao Mapa"
-            onPress={handleCompleteExercise}
-            bgColor="#f9a826b3"
-          />
-        ))}
+			{/* Conditional Rendering for Buttons after timer finishes */}
+			{timerFinished &&
+				(lastCheckpoint ? (
+					<Button
+						title="Finish Game"
+						onPress={handleCompleteExercise}
+						bgColor="#0aa812"
+					/>
+				) : (
+					<Button
+						title="Collect letter"
+						onPress={handleCompleteExercise}
+						bgColor="#f9a826b3"
+					/>
+				))}
 
-      {/* Show Start Challenge button if challenge isn't started */}
-      {!challengeStarted && (
-        <Button
-          title="Start Challenge"
-          onPress={startChallenge}
-          bgColor="#0aa812"
-        />
-      )}
-    </SafeAreaView>
-  );
+			{/* Show Start Challenge button if challenge isn't started */}
+			{!challengeStarted && (
+				<Button
+					title="Start Challenge"
+					onPress={startChallenge}
+					bgColor="#0aa812"
+				/>
+			)}
+		</SafeAreaView>
+	);
 }
